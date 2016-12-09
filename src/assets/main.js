829d274bc35f71202956b8b179ea14b6f8b5f2c4
Sonny Booth <s.booth@prospects.ac.uk>
2016-12-09 11:14:01 +0000

    Changes to main.js

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,38 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/GPFrontend.json',
+    dataType: 'jsonp',
+    success: function(res) {
+      console.log(res);
+      addCourses(res.courses.completed);
+    }
+  });
+
+  function addCourses(courses) {
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+      var $course = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        href: course.url,
+        target: '_blank',
+        text: 'See Course',
+        'class': 'btn btn-primary'
+      }).appendTo($course);
+
+    })
+  }
 
 });
